ea15f3fe0d2a9e3c424695642b5e8f950248f086
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-04-24 14:49:37 +0300

    contacts

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,96 @@
+import HttpError from "../helpers/HttpError.js";
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const contacts = await contactsService.listContacts();
+  if (!contacts.length) {
+    throw HttpError(404, "Contacts not found");
+  }
+  res.status(200).json({
+    status: "success",
+    code: 200,
+    data: { contacts },
+  });
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const contact = await contactsService.getContactById(id);
+  if (!contact) {
+    res.status(404).json({
+      message: "Not found",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    code: 200,
+    data: contact,
+  });
+};
 
 export const deleteContact = (req, res) => {};
 
 export const createContact = (req, res) => {};
 
 export const updateContact = (req, res) => {};
+
+//  {
+//     "id": "AeHIrLTr6JkxGE6SN-0Rw",
+//     "name": "Allen Raymond",
+//     "email": "nulla.ante@vestibul.co.uk",
+//     "phone": "(992) 914-3792"
+//   },
+//   {
+//     "id": "qdggE76Jtbfd9eWJHrssH",
+//     "name": "Chaim Lewis",
+//     "email": "dui.in@egetlacus.ca",
+//     "phone": "(294) 840-6685"
+//   },
+//   {
+//     "id": "drsAJ4SHPYqZeG-83QTVW",
+//     "name": "Kennedy Lane",
+//     "email": "mattis.Cras@nonenimMauris.net",
+//     "phone": "(542) 451-7038"
+//   },
+//   {
+//     "id": "vza2RIzNGIwutCVCs4mCL",
+//     "name": "Wylie Pope",
+//     "email": "est@utquamvel.net",
+//     "phone": "(692) 802-2949"
+//   },
+//   {
+//     "id": "05olLMgyVQdWRwgKfg5J6",
+//     "name": "Cyrus Jackson",
+//     "email": "nibh@semsempererat.com",
+//     "phone": "(501) 472-5218"
+//   },
+//   {
+//     "id": "1DEXoP8AuCGYc1YgoQ6hw",
+//     "name": "Abbot Franks",
+//     "email": "scelerisque@magnis.org",
+//     "phone": "(186) 568-3720"
+//   },
+//   {
+//     "id": "Z5sbDlS7pCzNsnAHLtDJd",
+//     "name": "Reuben Henry",
+//     "email": "pharetra.ut@dictum.co.uk",
+//     "phone": "(715) 598-5792"
+//   },
+//   {
+//     "id": "C9sjBfCo4UJCWjzBnOtxl",
+//     "name": "Simon Morton",
+//     "email": "dui.Fusce.diam@Donec.com",
+//     "phone": "(233) 738-2360"
+//   },
+//   {
+//     "id": "e6ywwRe4jcqxXfCZOj_1e",
+//     "name": "Thomas Lucas",
+//     "email": "nec@Nulla.com",
+//     "phone": "(704) 398-7993"
+//   },
+//   {
+//     "id": "rsKkOQUi80UsgVPCcLZZW",
+//     "name": "Alec Howard",
+//     "email": "Donec.elementum@scelerisquescelerisquedui.net",
+//     "phone": "(748) 206-2688"
+//   }
---
+++ services/contactsServices.js
@@ -0,0 +1,50 @@
+import path from "node:path";
+import * as fs from "node:fs/promises";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+async function writeContacts(contacts) {
+  const data = await fs.writeFile(
+    contactsPath,
+    JSON.stringify(contacts, null, 2)
+  );
+  return data;
+}
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  return JSON.parse(data);
+}
+
+async function getContactById(id) {
+  const contacts = await listContacts();
+  const contact = contacts.find((item) => item.id === id);
+  if (typeof contact === "undefined") {
+    return null;
+  }
+  return contact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  contacts.push(newContact);
+  await writeContacts(contacts);
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+
+  const removeContact = contacts[index];
+  contacts.splice(index, 1);
+  await writeContacts(contacts);
+  return removeContact;
+}
+
+export default { getContactById, removeContact, addContact, listContacts };
