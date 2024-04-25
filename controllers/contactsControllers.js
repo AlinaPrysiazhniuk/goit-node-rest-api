02d21e600f135d5cdaa62eb2b705d176505ea2f1
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-04-25 17:01:39 +0300

    error

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,7 +1,7 @@
 import HttpError from "../helpers/HttpError.js";
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = async (req, res) => {
+export const getAllContacts = async (req, res, next) => {
   try {
     const contacts = await contactsService.listContacts();
     if (!contacts.length) {
@@ -13,12 +13,11 @@ export const getAllContacts = async (req, res) => {
       data: { contacts },
     });
   } catch (error) {
-    console.log(error.message);
-    res.status(error.status).send(error.message);
+    next(error);
   }
 };
 
-export const getOneContact = async (req, res) => {
+export const getOneContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const contact = await contactsService.getContactById(id);
@@ -31,27 +30,28 @@ export const getOneContact = async (req, res) => {
       data: contact,
     });
   } catch (error) {
-    console.log(error.message);
-    res.status(error.status).send(error.message);
+    next(error);
   }
 };
 
-export const deleteContact = async (req, res) => {
-  const { id } = req.params;
-  const contact = await contactsService.removeContact(id);
-  if (!contact) {
-    res.status(404).json({
-      message: "Not found",
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await contactsService.removeContact(id);
+    if (!contact) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      data: contact,
     });
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json({
-    status: "success",
-    code: 201,
-    data: contact,
-  });
 };
 
-export const createContact = async (req, res) => {
+export const createContact = async (req, res, next) => {
   const { name, email, phone } = req.body;
   const contact = await contactsService.addContact(name, email, phone);
   res.status(200).json({
@@ -62,64 +62,3 @@ export const createContact = async (req, res) => {
 };
 
 export const updateContact = (req, res) => {};
-
-//  {
-//     "id": "AeHIrLTr6JkxGE6SN-0Rw",
-//     "name": "Allen Raymond",
-//     "email": "nulla.ante@vestibul.co.uk",
-//     "phone": "(992) 914-3792"
-//   },
-//   {
-//     "id": "qdggE76Jtbfd9eWJHrssH",
-//     "name": "Chaim Lewis",
-//     "email": "dui.in@egetlacus.ca",
-//     "phone": "(294) 840-6685"
-//   },
-//   {
-//     "id": "drsAJ4SHPYqZeG-83QTVW",
-//     "name": "Kennedy Lane",
-//     "email": "mattis.Cras@nonenimMauris.net",
-//     "phone": "(542) 451-7038"
-//   },
-//   {
-//     "id": "vza2RIzNGIwutCVCs4mCL",
-//     "name": "Wylie Pope",
-//     "email": "est@utquamvel.net",
-//     "phone": "(692) 802-2949"
-//   },
-//   {
-//     "id": "05olLMgyVQdWRwgKfg5J6",
-//     "name": "Cyrus Jackson",
-//     "email": "nibh@semsempererat.com",
-//     "phone": "(501) 472-5218"
-//   },
-//   {
-//     "id": "1DEXoP8AuCGYc1YgoQ6hw",
-//     "name": "Abbot Franks",
-//     "email": "scelerisque@magnis.org",
-//     "phone": "(186) 568-3720"
-//   },
-//   {
-//     "id": "Z5sbDlS7pCzNsnAHLtDJd",
-//     "name": "Reuben Henry",
-//     "email": "pharetra.ut@dictum.co.uk",
-//     "phone": "(715) 598-5792"
-//   },
-//   {
-//     "id": "C9sjBfCo4UJCWjzBnOtxl",
-//     "name": "Simon Morton",
-//     "email": "dui.Fusce.diam@Donec.com",
-//     "phone": "(233) 738-2360"
-//   },
-//   {
-//     "id": "e6ywwRe4jcqxXfCZOj_1e",
-//     "name": "Thomas Lucas",
-//     "email": "nec@Nulla.com",
-//     "phone": "(704) 398-7993"
-//   },
-//   {
-//     "id": "rsKkOQUi80UsgVPCcLZZW",
-//     "name": "Alec Howard",
-//     "email": "Donec.elementum@scelerisquescelerisquedui.net",
-//     "phone": "(748) 206-2688"
-//   }
