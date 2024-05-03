4bded07047a3d043b7abbfecb65cf6cfef822ae8
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-05-03 23:01:24 +0300

    error

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -49,6 +49,9 @@ export const updateContact = async (req, res, next) => {
 
     const { id } = req.params;
     const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
     res.status(200).json(result);
   } catch (error) {
     next(error);
@@ -59,7 +62,9 @@ export const deleteContact = async (error, req, res, next) => {
   try {
     const { id } = req.params;
     const result = await Contact.findByIdAndDelete(id);
-
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
     res.status(200).json(result);
   } catch (error) {
     next(error);
