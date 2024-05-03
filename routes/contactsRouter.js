62c3485119d5e7c7fe9943d3e0c90d396dc070aa
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-05-03 16:51:08 +0300

    update

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -22,6 +22,6 @@ contactsRouter.post("/", createContact);
 
 contactsRouter.put("/:id", isValidId, updateContact);
 
-contactsRouter.patch("/:id/favorite", updateStatusContact);
+contactsRouter.patch("/:id/favorite", isValidId, updateStatusContact);
 
 export default contactsRouter;
