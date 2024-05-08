a609b2833651b85e429a36015b3586657b624b7a
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-05-08 21:43:35 +0300

    validate

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { register, login } from "../controllers/authControllers.js";
+import { registerSchema, loginSchema } from "../schemas/usersSchemas.js";
+import { validateBody } from "../middlewares/validateBody.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", register);
-authRouter.post("/login", login);
+authRouter.post("/register", validateBody(registerSchema), register);
+authRouter.post("/login", validateBody(loginSchema), login);
 
 export default authRouter;
--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -6,3 +6,10 @@ export const registerSchema = Joi.object({
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
     .required(),
 });
+
+export const loginSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+});
