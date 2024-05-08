ce492054e0d0e52295ea937d1702c877aa7fba96
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-05-08 21:30:29 +0300

    login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.js";
+import bcrypt from "bcrypt";
 
 export const register = async (req, res, next) => {
   const { email, password } = req.body;
@@ -6,13 +7,37 @@ export const register = async (req, res, next) => {
     const user = await User.findOne({ email: email });
 
     if (user !== null) {
-      return res.status(409).send("User already register");
+      return res.status(409).send("User already registered");
     }
 
-    const result = await User.create({ email, password });
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const result = await User.create({ email, password: passwordHash });
     console.log({ result });
-    res.send("register");
+    res.status(201).send("Registration succesfully");
   } catch (error) {
     next(error);
   }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user === null) {
+      return res.status(401).send("Email or password is incorrect");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch === false) {
+      return res.status(401).send("Email or password is incorrect");
+    }
+    res.send("Login");
+  } catch (error) {
+    next(error);
+  }
+
+  res.send("login");
 };
