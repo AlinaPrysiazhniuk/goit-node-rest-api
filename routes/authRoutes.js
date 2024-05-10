9e25063b3fe3d8b94294b5bc0ac68049c6efff76
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-05-10 13:44:06 +0300

    logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { register, login } from "../controllers/authControllers.js";
+import { register, login, logout } from "../controllers/authControllers.js";
 import { registerSchema, loginSchema } from "../schemas/usersSchemas.js";
 import { validateBody } from "../middlewares/validateBody.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(registerSchema), register);
 authRouter.post("/login", validateBody(loginSchema), login);
+authRouter.post("/logout", authenticate, logout);
 
 export default authRouter;
