945ca433d28b4e4b65457ca457468627a141d830
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-05-10 15:36:42 +0300

    function

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,7 +4,7 @@ import { Contact } from "../models/contacts.js";
 export const getAllContacts = async (req, res, next) => {
   try {
     const { id: owner } = req.user;
-    const { page = 1, limit = 12 } = req.query;
+    const { page = 1, limit = 20 } = req.query;
     const skip = (page - 1) * limit;
     const result = await Contact.find({ owner })
       .skip(skip)
